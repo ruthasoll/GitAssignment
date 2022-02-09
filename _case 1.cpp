739d6601dc43f7d8b18a9ffbf964a0e6b2480885
lidet5 <60505945+lidet5@users.noreply.github.com>
2022-02-09 16:27:05 +0300

    Add files via upload

---
+++ _case 1.cpp
@@ -0,0 +1,53 @@
+//Transformation case 1
+
+#include<iostream>
+#include<graphics.h>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int gd=DETECT,gm,s;
+    initgraph(&gd,&gm,(char*)"");
+    cout<<"1.Translation\n2.Rotation\n3.Scaling\n4.Reflection\n5.Shearing   "<<endl;
+    cout<<"Selection:";
+    cin>>s;
+    switch(s)
+        {
+        case 1:
+            {   int x1=200,y1=150,x2=300,y2=250;
+                int tx=50,ty=50;
+                cout<<"Rectangle before translation"<<endl;
+                setcolor(3);
+                rectangle(x1,y1,x2,y2);
+                setcolor(4);
+                cout<<"Rectangle after translation"<<endl;
+                rectangle(x1+tx,y1+ty,x2+tx,y2+ty);
+                getch();
+                break;
+            }
+        case 2:
+            {     
+			break;
+}
+        case 3:
+            {  
+			
+			break;
+}
+        case 4:
+            {    break;
+}
+        case 5:
+        {
+}
+default:
+          {
+            cout<<"Invalid Selection"<<endl;
+            break;
+          }
+        }
+closegraph();
+getch();
+    return 0;
+}
+

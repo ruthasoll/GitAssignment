6425e01e8a34bf038e48924b953eded3b6c32ba0
ruth22soft <ruthtamiru10@gmail.com>
2022-02-11 17:49:40 +0300

    changed the set color to 3

--- _case4.cpp
+++ _case4.cpp
@@ -61,7 +61,7 @@ int main()
 			
                  int x1=200,y1=300,x2=500,y2=300,x3=350,y3=400;
                  cout<<"triangle before reflection"<<endl;
-                 setcolor(3);
+                 setcolor(4);
                  line(x1,y1,x2,y2);
                  line(x1,y1,x3,y3);
                  line(x2,y2,x3,y3);
